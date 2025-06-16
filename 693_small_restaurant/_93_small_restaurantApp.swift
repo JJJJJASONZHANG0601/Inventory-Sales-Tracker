a0d3d17cc72fe68d109dc32e4b9be0a5279404ff
JJJJJASONZHANG0601 <zhangjiacheng020601@gmail.com>
2025-06-16 01:57:13 -0400

    Initial Commit

---
+++ 693_small_restaurant/_93_small_restaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  _93_small_restaurantApp.swift
+//  693_small_restaurant
+//
+//  Created by Jason Zhang on 2025-06-16.
+//
+
+import SwiftUI
+
+@main
+struct _93_small_restaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 693_small_restaurantTests/_93_small_restaurantTests.swift
@@ -0,0 +1,16 @@
+//
+//  _93_small_restaurantTests.swift
+//  693_small_restaurantTests
+//
+//  Created by Jason Zhang on 2025-06-16.
+//
+
+import Testing
+
+struct _93_small_restaurantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
